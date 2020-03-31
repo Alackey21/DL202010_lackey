c0df7f32809c09094227c8dc2aeaff8619aee5b0
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-03-31 08:48:41 -0500

    adding register and testbench

---
+++ Lab 9/Lab_9.srcs/sim_1/new/register_test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2020 08:30:13 AM
+// Design Name: 
+// Module Name: register_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_test(
+
+    );
+endmodule
