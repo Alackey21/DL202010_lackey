ff9649ecb3c5c0ec95970a913dad3670f878dbcb
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-09 16:19:33 -0500

    more changes

--- Lab10/Lab10.srcs/sim_2/new/sseg4_TDM_test.sv
+++ Lab10/Lab10.srcs/sim_2/new/sseg4_TDM_test.sv
@@ -17,6 +17,16 @@ module sseg4_TDM_test();
     end 
     initial begin
         data = 16'b0000010100110001; #5
+        clk =0; en =0; rst =0; D =4 ' h0 ; #7;
+        rst = 1; #3; // reset
+        D = 4 ' hA ; en = 1; rst = 0; #10;
+        D = 4 ' h3 ; #2;
+        en = 0; #5;
+        en = 1; #3;
+        D = 4 ' h0 ; #2;
+        en = 0; #10;
+        en = 1; #2;
+        D = 4 ' h6 ; #11;
         $finish;
     end
 
