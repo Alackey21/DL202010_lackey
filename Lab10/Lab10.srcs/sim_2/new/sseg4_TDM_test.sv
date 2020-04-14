86c09b681ec49e1b27cd0aabd9cdbba265672c0a
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-14 02:51:43 -0500

    closing changes

--- Lab10/Lab10.srcs/sim_2/new/sseg4_TDM_test.sv
+++ Lab10/Lab10.srcs/sim_2/new/sseg4_TDM_test.sv
@@ -16,16 +16,16 @@ module sseg4_TDM_test();
     clock = ~clock; #5;
     end 
     
-    //integer i;
+    integer i;
     
     initial begin
         data = 16'h0000; clock = 0; reset =0; #7;
         reset = 1; #3; //  reset
-        /*data = 16'h1234;*/ reset = 0; hex_dec = 0; sign = 0; #5;
-        /*for ( i =0; i <= 60000; i = i +1)begin 
-            hex_dec = 0; sign = 1; #5; 
-        end*/
-        hex_dec = 0; sign = 0; #1000000
+        data = 16'h1234;#5
+        reset = 0; hex_dec = 0; sign = 0; #5;
+        for ( i =0; i <= 100000; i = i +1)begin 
+            hex_dec = 0; sign = 0; #5; 
+        end
         $finish;
     end
 
