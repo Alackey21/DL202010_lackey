6eb50f3c9324901dd1897d648a86420dfdce9ed1
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-20 18:55:05 -0500

    opening project and debounce

---
+++ Lab11/Lab11.srcs/sources_1/new/Lab11_guess.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2020 06:40:17 PM
+// Design Name: 
+// Module Name: Lab11_guess
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Lab11_guess(
+
+    );
+endmodule
