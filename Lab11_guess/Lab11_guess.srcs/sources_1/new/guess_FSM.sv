6669add0f81f976525b09e0ff7c96a87e377e347
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-21 10:31:59 -0500

    guess test building

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
-
-module guess_FSM(input clk, reset,
-    input in,
+// ELC 2137,  Ashlie Lackey, 2020 -04 -21
+module guess_FSM#(parameter N = 21)(input clk, reset,
+    input [3:0] b,
     output reg [3:0] y,
     output reg win,
     output reg lose
@@ -17,31 +17,68 @@ module guess_FSM(input clk, reset,
     
     reg [2:0] state, state_next;
     
-    case (state)
-    s0: begin
-        y[0] = 1;
-        if
+    always_ff @(posedge clk or posedge reset)
+        if(reset) begin
+            state <= s0;
+        end
+        else begin
+            state <= state_next;
+        end
+        
+    always_comb  begin//  default  behavior
+    state_next = state;
+    //tick = 0;
+    
+        case (state)
+            s0: begin
+                y[0] = 1;
+                if (~b[3] & ~b[2] & ~b[1] & b[0])
+                    state_next = swin;
+                else if (b[3] | b[2] | b[1])
+                    state_next = lose;
+            end
+            
+            s1: begin
+                y[1] = 1;
+                if (~b[3] & ~b[2] & b[1] & ~b[0])
+                    state_next = swin;
+                else if (b[3] | b[2] | b[0])
+                    state_next = lose;
+            end
+            
+            s2: begin
+                y[2] = 1;
+                if (~b[3] & b[2] & ~b[1] & ~b[0])
+                    state_next = swin;
+                else if (b[3] | b[1] | b[0])
+                    state_next = lose;
+            end
+            
+            s3: begin
+                y[3] = 1;
+                if (~b[3] & ~b[2] & ~b[1] & b[0])
+                    state_next = swin;
+                else if (b[2] | b[1] | b[0])
+                    state_next = lose;
+                else if (~b[3] & ~b[2] & ~b[1] & ~b[0])
+                    state_next = s0;
+            end
+            
+            swin: begin
+                win = 1;
+                if (~b[3] & ~b[2] & ~b[1] & ~b[0])
+                    state_next = s0;
+                else if (b[3] | b[2] | b[1] | b[0])
+                    state_next = state;
+            end
+            
+            slose: begin
+                lose = 1;
+                if (~b[3] & ~b[2] & ~b[1] & ~b[0])
+                    state_next = s0;
+                else if (b[3] | b[2] | b[1] | b[0])
+                    state_next = state;
+            end
+        endcase
     end
-    
-    s1: begin
-        y[1] = 1;
-    end
-    
-    s2: begin
-    
-    end
-    
-    s3: begin
-    
-    end
-    
-    swin: begin
-    
-    end
-    
-    slose: begin
-    
-    end
-    
-    
 endmodule
