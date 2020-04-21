855a38cc0a83f5d64c508e4e968817b9bc19e04a
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-20 23:56:51 -0500

    buidling guess_FSM

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -2,29 +2,29 @@
 
 module guess_FSM(input clk, reset,
     input in,
-    output reg out,
-    output reg tick
+    output reg [3:0] y,
+    output reg win,
+    output reg lose
     );
     
-    localparam[2:0]
-        so = 3'b000;
-        s1 = 3'b001;
-        s2 = 3'b010;
-        s3 = 3'b011;
-        swin = 3'b100;
+    localparam [2:0]
+        s0 = 3'b000,
+        s1 = 3'b001,
+        s2 = 3'b010,
+        s3 = 3'b011,
+        swin = 3'b100,
         slose = 3'b101;
     
     reg [2:0] state, state_next;
     
-    
     case (state)
     s0: begin
-        y0 = 1;
+        y[0] = 1;
         if
     end
     
     s1: begin
-    
+        y[1] = 1;
     end
     
     s2: begin
