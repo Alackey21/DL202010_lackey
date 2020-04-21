a7cb6fdc260233bd16f2a374896ea1e50807d57b
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-21 15:13:35 -0500

    more work of guess_FSM test

--- Lab11_guess/Lab11_guess.srcs/sim_1/new/guess_FSM_test.sv
+++ Lab11_guess/Lab11_guess.srcs/sim_1/new/guess_FSM_test.sv
@@ -2,12 +2,12 @@
 // ELC 2137,  Ashlie Lackey, 2020 -04 -21
 module guess_FSM_test();
 
-reg clk, reset;
-reg [3:0] b;
-wire [3:0]y;
-wire win, lose;
-
-guess_FSM#(.N(2)) gFSM (.clk(clk), .reset(reset),.b(clk), .y(y), .win(win), .lose(lose));
+    reg clk, reset;
+    reg [3:0] b;
+    wire [3:0]y;
+    wire win, lose;
+    
+    guess_FSM#(.N(2)) gFSM (.clk(clk), .reset(reset),.b(clk), .y(y), .win(win), .lose(lose));
 
 //module  debounce_test ();
     //reg clk , reset , in;
@@ -20,21 +20,29 @@ guess_FSM#(.N(2)) gFSM (.clk(clk), .reset(reset),.b(clk), .y(y), .win(win), .los
     end
     
     initial  begin
-        clk =0;  reset =0; in=0;  #5;
+        clk =0;  reset =0; b=4'b0000;  #5;
         reset =1;  #10;
         reset =0; #5;
-        //  bounce
-        for (i=0; i<10; i=i+1)  begin
-            #20 in=~in;
+        
+        //  cycle through states
+        for (i=0; i<4; i=i+1)  begin
+            #20 y[i]=~y[i];
         end
-        // hold  input = 1 for a while
-        in = 1; #200;
-        //  bounce
-        for (i=0; i<10; i=i+1)  begin
-            #20 in=~in;
+        
+        b[0] = 1; #200;
+        
+        b[1] = 1; #200;
+        
+        b[2] = 1; #200;
+        
+        b[3] = 1; #200;
+        
+        for (i=0; i<4; i=i+1)  begin
+            #20 y[i]=~y[i];
         end
+        
         // hold  input = 0 for a while
-        in = 0; #200;
+        b = 4'b0000; #200;
         $finish;
     end
 endmodule
