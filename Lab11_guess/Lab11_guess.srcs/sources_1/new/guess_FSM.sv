5db84ecdf6b896927e230fb404549291e0e6cc09
Alackey21 <59978830+Alackey21@users.noreply.github.com>
2020-04-20 22:46:37 -0500

    lab11_guess

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -1,26 +1,47 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/20/2020 06:56:39 PM
-// Design Name: 
-// Module Name: guess_FSM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module guess_FSM(
-
+module guess_FSM(input clk, reset,
+    input in,
+    output reg out,
+    output reg tick
     );
+    
+    localparam[2:0]
+        so = 3'b000;
+        s1 = 3'b001;
+        s2 = 3'b010;
+        s3 = 3'b011;
+        swin = 3'b100;
+        slose = 3'b101;
+    
+    reg [2:0] state, state_next;
+    
+    
+    case (state)
+    s0: begin
+        y0 = 1;
+        if
+    end
+    
+    s1: begin
+    
+    end
+    
+    s2: begin
+    
+    end
+    
+    s3: begin
+    
+    end
+    
+    swin: begin
+    
+    end
+    
+    slose: begin
+    
+    end
+    
+    
 endmodule
